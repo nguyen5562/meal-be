4583c1062d01fd0742471d5c98d82fc8a7536289
nguyen5562 <nknguyen112345@gmail.com>
2026-07-29 21:16:31 +0700

    feat: initialize NestJS application with CORS configuration and environment variables

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://meal.nguyenkhoi.io.vn',
+    ],
+  });
   console.log(process.env.PORT);
   console.log(process.env.DATABASE_URL);
   await app.listen(process.env.PORT || 4000);
